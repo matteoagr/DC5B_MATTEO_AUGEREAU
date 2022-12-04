e9727f5c32e06f4f83c7fd79665e12c5419a9146
matteo augereau <matteo.augereau1@gmail.com>
2022-12-04 20:43:40 +0100

    test exo 4

---
+++ EXO 4/ft_print_is_negative.js
@@ -0,0 +1,10 @@
+function exo4(n) {
+    if (num % 1 != 0)
+        return "veuillez rentrer un nombre entier"
+    if (n < 0) {
+        console.log('N')
+    } else {
+        console.log('P')
+    }
+}
+exo4()

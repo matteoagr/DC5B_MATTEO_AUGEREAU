3c35794102baf6e18b9d97581b6082a91f4a38b0
matteo augereau <matteo.augereau1@gmail.com>
2022-12-04 20:03:54 +0100

    New script

--- EXO 1/ft_print_alphabet.js
+++ EXO 1/ft_print_alphabet.js
@@ -1,5 +1,8 @@
-function alphabetFunction() {
-    let alphabet = 'abcdefghijklmnopqrstuvwxyz';
-    console.log(alphabet);
-};
-alphabetFunction()
+function printAlphabet() {
+    let alphabet = ''
+    for (i = 9; ++i < 36;) {
+        alphabet += i.toString(36);
+    }
+    console.log(alphabet)
+}
+printAlphabet()

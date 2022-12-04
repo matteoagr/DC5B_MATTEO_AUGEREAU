e01571f453c64b58b817913c561a684b15b2fcd5
matteo augereau <matteo.augereau1@gmail.com>
2022-12-04 20:58:11 +0100

    Exo 5

---
+++ EXO 5/ft_print_comb.js
@@ -0,0 +1,19 @@
+function exo5() {
+    let res = []
+    for (let i = 0; i < 1000; i++) {
+        let n = i
+        let first = n % 10;
+        n = n - first;
+        let second = n % 100;
+        n = n - second;
+        second = second / 10
+        let third = n % 1000 / 100;
+        if (first != second && first != third && second != third) {
+            if (third < second && second < first) {
+                res.push(i);
+            }
+        }
+    }
+    console.log(res);
+}
+exo5()
